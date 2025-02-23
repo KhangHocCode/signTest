3c50880e530d6b444737110e8abfa083273e6710
KhangHocCode <dinhn5687@gmail.com>
2025-02-23 22:10:38 +0700

    update

--- app.js
+++ app.js
@@ -10,26 +10,21 @@ sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
-// Toggle password visibility for sign-in form
 const toggleSignInPassword = document.querySelector('#toggleSignInPassword');
 const signInPassword = document.querySelector('#sign-in-password');
 
 toggleSignInPassword.addEventListener('click', function (e) {
-  // toggle the type attribute
   const type = signInPassword.getAttribute('type') === 'password' ? 'text' : 'password';
   signInPassword.setAttribute('type', type);
   // toggle the eye icon
   this.classList.toggle('ri-eye-off-line');
 });
 
-// Toggle password visibility for sign-up form
 const toggleSignUpPassword = document.querySelector('#toggleSignUpPassword');
 const signUpPassword = document.querySelector('#sign-up-password');
 
 toggleSignUpPassword.addEventListener('click', function (e) {
-  // toggle the type attribute
   const type = signUpPassword.getAttribute('type') === 'password' ? 'text' : 'password';
   signUpPassword.setAttribute('type', type);
-  // toggle the eye icon
   this.classList.toggle('ri-eye-off-line');
 });
